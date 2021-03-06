832591117e448cb655c24c58c6c6008fae8b70db
hamzaali <hamza54834@gmail.com>
2021-03-06 12:50:28 +0500

    Payment: Working Data

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useStateValue } from "../../StateProvider";
+import CheckoutProduct from './../CheckoutProduct/CheckoutProduct';
 import "./payment.css";
 
 function Payment() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="payment">
       <div className="payment__container">
@@ -10,10 +14,28 @@ function Payment() {
               <div className="payment__title">
                   <h3>Delivery Address</h3>
               </div>
+              <div className="payment__address">
+                  <p>{user?.email}</p>
+                  <p>123 React Lane</p>
+                  <p>Pakistan, Karachi</p>
+              </div>
             </div>
              {/* Payment section - Reviews Items */}
         <div className="payment__section">
-            
+                <div className="payment__title">
+                    <h3>Review items and delivery</h3>
+                </div>
+                <div className="payment__item">
+                    {basket.map((item)=> (
+                        <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                        />
+                    ))}
+                </div>
             </div>
             {/* Payment section - Payment method */}
         <div className="payment__section">

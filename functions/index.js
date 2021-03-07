9e8142f57df9f71a43c7b3c51f159c27fcea2242
hamzaali <hamza54834@gmail.com>
2021-03-07 20:07:51 +0500

    Issues: cloud function fix

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,26 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("pk_test_51IPbKHLGi8vxXfkLOAPosAJ7j4HUk6dmKfIqlgMAr3AvetVs30fUOpnpd8tO2ET9FVtjlMYWPmmvAViQ8HYrnftP00GeZgjBqQ");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
+// ~ API
+// ~ API config
+const app = express();
+
+// ~ Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// ~ API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+// ~ listen command
+exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/clone-2fd2a/us-central1/api

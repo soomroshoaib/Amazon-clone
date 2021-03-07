330863effd0b21da11df67d2b02e9b865bef57a2
hamzaali <hamza54834@gmail.com>
2021-03-07 23:47:01 +0500

    Stripe: Working

--- functions/index.js
+++ functions/index.js
@@ -18,6 +18,19 @@ app.use(express.json());
 
 // ~ API routes
 app.get("/", (request, response) => response.status(200).send("hello world"));
+app.post("/payments/create", async (request, response)=> {
+    const total = request.query.total;
+    console.log('Payment Request Recieved BOOM!!! for this amount >>>', total);
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,  // subunits of the currency
+        currency: "usd"
+    });
+
+    // Ok - Created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+} )
 
 // ~ listen command
 exports.api = functions.https.onRequest(app);
